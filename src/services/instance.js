8b8b6c4bd8c1f8cb54627ea940e3e3a9c6dd956c
VinodhGangadharan <vinodhgangadharan1992@gmail.com>
2024-11-11 15:17:43 +0530

    added render url

--- src/services/instance.js
+++ src/services/instance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // define the base url
 // const baseUrl = 'https://node-fsd61wde.onrender.com';
-const baseUrl = 'http://localhost:3001';
-
+//const baseUrl = 'http://localhost:3001';
+const baseUrl = 'https://cashflow-backend-5i5l.onrender.com';
 // define the axios instance
 const instance = axios.create({
     baseURL: baseUrl,
